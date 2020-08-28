9c16d184a74f0661937b8e88f6af378087e31403
cjhcube <50494860+cjhcube@users.noreply.github.com>
2020-08-29 00:35:42 +0100

    Initial upload

---
+++ assets/scripts/data.js
@@ -0,0 +1,27 @@
+/* 
+PBL Manager
+An online tool to store and train Square-1 PBL algs.
+v2.0.0
+
+Original author: Charlie Harrison.
+
+Released under the MIT License.
+*/
+
+function loadPbls(callback) {
+    if (localStorage.getItem("pbls") === null) {
+        $.getJSON("assets/data/init.json", pbls => {
+            callback(pbls);
+            localStorage.setItem("pbls", JSON.stringify(pbls));
+        });
+        return;
+    } else {
+        let pbls = JSON.parse(localStorage.getItem("pbls"));
+        callback(pbls);
+        return;
+    }
+}
+
+function updatePbls(pbls) {
+    localStorage.setItem("pbls", JSON.stringify(pbls));
+}
